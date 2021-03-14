64cd04d3ea2aa1ce0b55e22df42bf299c122d84c
Clément Chapurlat <clemch2001@gmail.com>
2021-03-14 16:39:09 +0100

    Flatlist fonctionnelle, reliée aux data eleves

--- screens/homeScreen.js
+++ screens/homeScreen.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { View, Text, Button, StyleSheet, Image, FlatList } from 'react-native';
 import eleves from '../Listes/elevesData';
+import List_item from '../Components/List_item'
 
 class HomeScreen extends React.Component {
     render() {
         return (
             <View title='HomeScreen' style={{flex: 1}}>
 
-                    <FlatList
-                            data={eleves}
-                            keyExtractor={(item) => item.id.toString()}
-                            renderItem={({item}) => <Text>{item.name}, {item.description}</Text>} />
+                <FlatList
+                    contentContainerStyle={styles.flatstyle}
+                    numColumns={2}
+                    data={eleves}
+                    keyExtractor={(item) => item.id.toString()}
+                    renderItem={({item}) => <List_item eleve={item}/>} />
 
-                <View style={styles.container}>
+                <View style={styles.BotBar}>
                     <Text> Home Screen test </Text>
                     <Button title='aller aux details' onPress={() => this.props.navigation.navigate('Details')} />
                 </View>
@@ -25,17 +28,12 @@ class HomeScreen extends React.Component {
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-    container: {
+    flatstyle: {
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    BotBar: {
         alignItems: 'center',
         justifyContent: 'center',
     },
-    listeImage: {
-        flex:1,
-    },
-    sizeImage:{
-        flex: 1,
-        width: 200,
-        height: 200,
-        resizeMode:'contain',
-    }
 });
